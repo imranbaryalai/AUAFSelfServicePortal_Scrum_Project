c1bbeaec2b48ed19e8ec04426fbbd94b6ec60088
imranbaryalai <69864244+imranbaryalai@users.noreply.github.com>
2022-04-15 16:51:09 -0400

    Splash Screen Designed and Developed

---
+++ app/src/main/java/com/nextgendevs/auaf_stu_adv_app/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.nextgendevs.auaf_stu_adv_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+    }
+}
