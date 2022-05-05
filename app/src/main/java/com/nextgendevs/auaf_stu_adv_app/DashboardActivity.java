946951f62041549f895c470fbd6e982b520094a9
imranbaryalai <69864244+imranbaryalai@users.noreply.github.com>
2022-05-05 18:44:06 +0430

    Dashboard UI Designed (Tablet, Phone and Landscape Variations)
    1) Dashboard UI Designed
    2) Create the Tablet Version
    3) Landscape Version
    4) Launch Dashboard Activity after Sign In Successful
    5) Minor Design Changes and Bug Fixes

---
+++ app/src/main/java/com/nextgendevs/auaf_stu_adv_app/DashboardActivity.java
@@ -0,0 +1,14 @@
+package com.nextgendevs.auaf_stu_adv_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard_activity);
+    }
+}
--- app/src/main/java/com/nextgendevs/auaf_stu_adv_app/LoginActivity.kt
+++ app/src/main/java/com/nextgendevs/auaf_stu_adv_app/LoginActivity.kt
@@ -9,15 +9,35 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatImageButton
+import com.google.android.material.textfield.TextInputEditText
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
 
+
+
+
+
         val loginBtn = findViewById<Button>(R.id.loginBtn)
         loginBtn.setOnClickListener {
-            Toast.makeText(this@LoginActivity, "Login Button Clicked", Toast.LENGTH_SHORT).show()
+
+            val emailTextEdit = findViewById<TextInputEditText>(R.id.emailInput)
+            val passTextEdit = findViewById<TextInputEditText>(R.id.passwordInput)
+
+            val emailInput = emailTextEdit.text.toString()
+            val passwordInput = passTextEdit.text.toString()
+
+            println("Email: $emailInput")
+            println("Password: $passwordInput")
+
+            val intent = Intent(this@LoginActivity, DashboardActivity::class.java)
+            if (emailInput.equals("admin") && passwordInput.equals("admin")){
+                startActivity(intent)
+                finish()
+            }
+
         }
 
         val registerBtn = findViewById<TextView>(R.id.registerBtn)
